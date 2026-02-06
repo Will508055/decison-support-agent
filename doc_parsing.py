d3ef65b215a5131dda89c285f4a15d3c6fb0d82f
Will Sheriff <willsheriff43@gmail.com>
2026-02-06 14:19:41 -0500

    html parsing

--- doc_parsing.py
+++ doc_parsing.py
@@ -1,11 +1,20 @@
 from bs4 import BeautifulSoup
 
-def parse_night_riding_html() -> str:
-    file_path = 'documentation/Motorcycle_Night_Riding_by_Road_Guardians.html'
-
-    with open(file_path, 'r', encoding='utf-8') as file:
-        html_content = file.read()
+def parse_html_documents() -> list[str]:
+    file_classes_map = {'Motorcycle_Night_Riding_by_Road_Guardians.html': 'infinite-content-container infinite-container',
+                        'Cornering_101.html': 'post hentry uncustomized-post-template',
+                        'Tips_for_Cornering.html': 'blog__article__content rte',
+                        'Navigating_Blind_Turns.html': 'entry-content'}
     
-    soup = BeautifulSoup(html_content, "html.parser")
-    text = soup.find(class_='infinite-content-container infinite-container').get_text(strip=True)
-    return text
+    parsed_texts = []
+
+    for key, value in file_classes_map.items():
+        path = f'documentation/{key}'
+        with open(path, 'r', encoding='utf-8') as file:
+            html_content = file.read()
+    
+        soup = BeautifulSoup(html_content, "html.parser")
+        text = soup.find(class_=value).get_text(strip=True)
+        parsed_texts.append(text)
+
+    return parsed_texts
